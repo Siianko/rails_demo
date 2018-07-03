2e0292a3a06a61d4fbfb3ac301024dfc5351ab2b
Siianko <tsiyanko@gmail.com>
2018-07-03 17:22:30 +0200

    rspec tests went green

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Article, type: :model do
     end
 
     describe 'Validations' do
-        it { is_expected.to validate_presense_of :title }
-        it { is_expected.to validate_presense_of :content }
+        it { is_expected.to validate_presence_of :title }
+        it { is_expected.to validate_presence_of :content }
     end
 
     describe 'Factory' do
